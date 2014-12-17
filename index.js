decee4be5f0cffd32f91693b89641fd9412bc5dd
Marcio Cunha <marcio@winetag.com.br>
2014-12-17 14:31:48 -0200

    heroku

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(res, req) {
+	res.render('./home', { title: 'WineTag - The Wine World in Your Hand'});
 });
 
 app.listen(app.get('port'), function() {
